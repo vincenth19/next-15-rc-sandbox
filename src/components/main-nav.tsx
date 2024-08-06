c0d37280b3588e4f4fa9596fbf31c03e8a2d3687
Vincent Haryadi <vincenthary19@gmail.com>
2024-08-07 09:04:00 +1000

    feat: responsive navbar

--- src/components/main-nav.tsx
+++ src/components/main-nav.tsx
@@ -1,51 +1,85 @@
+import React from "react";
 import Link from "next/link";
+import { LucideHome, LucideMenu, LucideUsers } from "lucide-react";
 import {
   NavigationMenu,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
   navigationMenuTriggerStyle,
-} from "./ui/navigation-menu";
-import { LucideUsers } from "lucide-react";
+} from "@/components/ui/navigation-menu";
+import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 
 const MENUS = [
   {
     label: "Person App",
     href: "/",
+    icon: <LucideHome className="mr-2" />,
   },
   {
     label: "People",
     href: "/people",
+    icon: <LucideUsers className="mr-2" />,
   },
 ];
 
-const Navbar = () => {
+const Navbar: React.FC = () => {
   return (
     <div className="h-[4rem] border-b border-b-slate-200 border-solid">
       <div className="max-w-screen-2xl m-auto">
-        <div className="flex w-full px-10 py-5 gap-5 items-center h-[4rem]">
+        <div className="flex sm:hidden w-full px-5 py-5 gap-5 items-center h-[4rem]">
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button variant="ghost" size="icon" aria-label="Open menu">
+                <LucideMenu />
+              </Button>
+            </SheetTrigger>
+            <SheetContent side={"left"}>
+              <SheetHeader className="text-left">
+                <SheetTitle>Menu</SheetTitle>
+              </SheetHeader>
+              <div className="flex flex-col space-y-3 items-start mt-4">
+                {MENUS.map((menu) => (
+                  <SheetClose asChild key={menu.href}>
+                    <Link href={menu.href} className="flex w-full">
+                      <Button
+                        type="submit"
+                        variant={"ghost"}
+                        className="hover:bg-accent w-full justify-start"
+                      >
+                        {menu.icon}
+                        {menu.label}
+                      </Button>
+                    </Link>
+                  </SheetClose>
+                ))}
+              </div>
+            </SheetContent>
+          </Sheet>
+        </div>
+        <div className="hidden sm:flex w-full px-10 py-5 gap-5 items-center h-[4rem]">
           <NavigationMenu>
             <NavigationMenuList>
-              {MENUS.map((menu, idx) => {
-                return (
-                  <NavigationMenuItem>
-                    <Link
-                      href={menu.href}
-                      key={menu.href}
-                      className={"text-white"}
-                      legacyBehavior
-                      passHref
+              {MENUS.map((menu) => (
+                <NavigationMenuItem key={menu.href}>
+                  <Link href={menu.href} legacyBehavior passHref>
+                    <NavigationMenuLink
+                      className={navigationMenuTriggerStyle()}
                     >
-                      <NavigationMenuLink
-                        className={navigationMenuTriggerStyle()}
-                      >
-                        {idx === 0 ? <LucideUsers className="mr-2" /> : null}
-                        {menu.label}
-                      </NavigationMenuLink>
-                    </Link>
-                  </NavigationMenuItem>
-                );
-              })}
+                      {menu.icon}
+                      {menu.label}
+                    </NavigationMenuLink>
+                  </Link>
+                </NavigationMenuItem>
+              ))}
             </NavigationMenuList>
           </NavigationMenu>
         </div>
